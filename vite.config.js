b6293e64376e1392c86fd8dd378c3c07c4bfaf71
gunjanks <gunjanks24223@gmail.com>
2025-03-05 22:40:28 +0530

    Deploying Vite + React App

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/home/", // Replace with your GitHub repository name
+  base: "/portfolio/", // Replace with your GitHub repository name
 });
