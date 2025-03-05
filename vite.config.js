803cc8aa4d03d841012cb08d2787fbad99050e97
gunjanks <gunjanks24223@gmail.com>
2025-03-05 23:08:31 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/home/", // Replace with your GitHub repository name
+  base: "https://gunjanks.github.io/portfolio/home/", // Replace with your GitHub repository name
 });
